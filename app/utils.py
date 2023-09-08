0472ad36b7d39681e34cdd4a3e3aa56b3937e1de
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-09-08 16:54:24 +0000

    revert

--- app/utils.py
+++ app/utils.py
@@ -1,27 +1,54 @@
 import pandas as pd
 import streamlit as st
 
+
 def gather_mappings(desired_columns, input_columns):
-    """Gather mappings for desired columns from user input via Streamlit."""
+    """Gather mappings for desired columns from user input via Streamlit.
+    
+    Args:
+    - desired_columns (list): List of columns that user wants in the final dataframe.
+    - input_columns (list): List of columns available in the input dataframe.
+
+    Returns:
+    - dict: A mapping of desired columns to input columns based on user selection.
+    """
+    
     mappings = {}
+    
     for desired_col in desired_columns:
         value = st.selectbox(
             f"Map '{desired_col}' to:", 
             options=input_columns, 
             key=desired_col
         )
+        
         if value != "[Do not map]":
             mappings[desired_col] = value
+            
     return mappings
 
+
 def data_wrangling(df, mappings, desired_columns):
-    """Re-arrange and rename columns in the dataframe based on user mappings."""
+    """Re-arrange and rename columns in the dataframe based on user mappings.
+    
+    Args:
+    - df (DataFrame): Input dataframe.
+    - mappings (dict): Mapping of desired columns to input columns.
+    - desired_columns (list): List of columns that user wants in the final dataframe.
+
+    Returns:
+    - DataFrame: A dataframe with columns renamed and re-arranged based on user input.
+    """
+    
     df = df.rename(columns=mappings)
+    
     for col in desired_columns:
         if col not in df.columns:
             if col in mappings:
                 df[col] = df[mappings[col]]
             else:
                 df[col] = pd.NA
+                
     df = df[desired_columns]
+    
     return df
