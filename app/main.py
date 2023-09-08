9095b5581a3f3d3fbc771b5596d9c97c86b1a5ad
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-09-08 17:06:49 +0000

    revert

--- app/main.py
+++ app/main.py
@@ -2,38 +2,31 @@ import streamlit as st
 import pandas as pd
 from utils import gather_mappings, data_wrangling
 
+@st.cache(allow_output_mutation=True)
+def load_file(file):
+    return pd.read_excel(file)
+
 def run_app():
     """Run the Streamlit app for interactive column mapping."""
     
-    # Set the app title
     st.title("Interactive Column Mapping")
     
-    # Upload Excel file
     uploaded_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
     
     if uploaded_file:
-        df = pd.read_excel(uploaded_file)
+        df = load_file(uploaded_file)
         
-        # Get desired columns input from user using a text_area
         desired_cols_str = st.text_area("Enter desired columns separated by commas:", value="")
-        
-        # Parse the entered columns to create a list of desired columns
         desired_columns = [col.strip() for col in desired_cols_str.split(",") if col.strip()]
         
-        # If there are any desired columns entered
         if desired_columns:
-            # Combine default option with dataframe columns
             input_columns = ["[Do not map]"] + list(df.columns)
-            
-            # Gather the column mappings using the provided function
             mappings = gather_mappings(desired_columns, input_columns)
             
-            # If user submits the mappings, display the processed dataframe
-            if st.button("Submit Mapping"):
+            if st.button("Submit All Mappings"):
                 with st.spinner('Processing data...'):
                     df_out = data_wrangling(df, mappings, desired_columns)
-                    st.write(df_out.head())
+                    st.write(df_out.head())  # Display only the first few rows
 
-# Run the app if the script is executed directly
 if __name__ == "__main__":
     run_app()
--- app/utils.py
+++ app/utils.py
@@ -1,54 +1,28 @@
 import pandas as pd
 import streamlit as st
 
-
 def gather_mappings(desired_columns, input_columns):
-    """Gather mappings for desired columns from user input via Streamlit.
-    
-    Args:
-    - desired_columns (list): List of columns that user wants in the final dataframe.
-    - input_columns (list): List of columns available in the input dataframe.
-
-    Returns:
-    - dict: A mapping of desired columns to input columns based on user selection.
-    """
-    
+    """Gather mappings for desired columns from user input via Streamlit."""
     mappings = {}
-    
     for desired_col in desired_columns:
         value = st.selectbox(
             f"Map '{desired_col}' to:", 
             options=input_columns, 
             key=desired_col
         )
-        
         if value != "[Do not map]":
             mappings[desired_col] = value
-            
     return mappings
 
-
+@st.cache
 def data_wrangling(df, mappings, desired_columns):
-    """Re-arrange and rename columns in the dataframe based on user mappings.
-    
-    Args:
-    - df (DataFrame): Input dataframe.
-    - mappings (dict): Mapping of desired columns to input columns.
-    - desired_columns (list): List of columns that user wants in the final dataframe.
-
-    Returns:
-    - DataFrame: A dataframe with columns renamed and re-arranged based on user input.
-    """
-    
+    """Re-arrange and rename columns in the dataframe based on user mappings."""
     df = df.rename(columns=mappings)
-    
     for col in desired_columns:
         if col not in df.columns:
             if col in mappings:
                 df[col] = df[mappings[col]]
             else:
                 df[col] = pd.NA
-                
     df = df[desired_columns]
-    
     return df
